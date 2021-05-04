dff6621becd6872f1a7f7d79954f9696b1ad42a6
NuradinAhmed <Nuradinahmed2012@gmail.com>
2021-05-04 17:54:24 +0300

    added react routes for team path

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
-      <TeamPage/>
+      {/* This is our RouterTag that will allows us to define route that will contain a path */}
+      <Router>
+
+        <Route path="/teams">
+          <TeamPage/>
+        </Route>
+        
+      </Router>
     </div>
   );
 }
