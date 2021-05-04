b136f6bfba0606cd58fc80d9eae196593d24af7f
nuradin ahmed <Nuradinahmed2012@gmail.com>
2021-05-04 21:33:33 +0300

    Develop (#11)
    * Team API
    
    * React app with Team Page
    
    * added react routes for team path (#10)
    
    * added React Links to teamPage so its clickable from team to team match details

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
+
+      {/* This is our RouterTag that will allows us to define route that will contain a path */}
+      <Router>
+
+        <Route path="/teams/:teamName">
+          <TeamPage/>
+        </Route>
+        
+      </Router>
+
       <TeamPage/>
+
     </div>
   );
 }
--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,12 +1,22 @@
 import {React} from 'react';
 
+import { Link } from 'react-router-dom'; {/* Link allows us to click teamName and gets its detials card match */}
 
 //Accepting the match as a paramter here in the matchsmallcard
-export const MatchSmallCard = ({match}) => {
+export const MatchSmallCard = ({match, teamName}) => {
+  
+  if(!match) return null;
+  {/* apply same logic for getting only other team rather than bothteams*/}
+  const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
+
+  const otherTaamRoute = `/teams/${otherTeam}`;
+
   return (
     <div className="MatchSmallCard">
 
-      <p>{match.team1} vs {match.team2}</p>
+      <h3> vs <Link to={otherTaamRoute}>  {otherTeam}    </Link>   </h3>
+
+      <p> {match.matchWinner} won by {match.resultMargin} {match.result}</p>
 
     </div>
   );
