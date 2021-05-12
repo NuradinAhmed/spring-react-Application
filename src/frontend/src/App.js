cd762349721151f4644e4a8724e35347b201b170
NuradinAhmed <Nuradinahmed2012@gmail.com>
2021-05-12 11:14:57 +0300

    completed UI elements for Team Page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <div className="App">
       {/* This is our RouterTag that will allows us to define route that will contain a path */}
       <Router>
+        <Switch>
 
-        <Route path="/teams/:teamName">
-          <TeamPage/>
-        </Route>
+            <Route path="/teams/:teamName/matches/:year">
+              < MatchPage/>
+
+            </Route>
+
+            <Route path="/teams/:teamName">
+              <TeamPage/>
+            </Route>
+
+        </Switch>
         
       </Router>
     </div>
