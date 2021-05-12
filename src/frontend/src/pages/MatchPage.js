bf0feead38d2a3b47a6c6e7be2a8bac11fdbb70c
nuradin ahmed <Nuradinahmed2012@gmail.com>
2021-05-12 13:27:28 +0300

    Develop (#14)
    * Team API
    
    * React app with Team Page
    
    * added react routes for team path (#10)
    
    * added React Links to teamPage so its clickable from team to team match details
    
    * completed UI elements for Team Page
    
    * Add Backend Match API
    
    * added state effect in the front end

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -5,17 +5,43 @@ import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
     
-    
+
+    //going to define a state for the match API call: am saying my state = matches and the method calling to that state = setMatches
+    const [matches, setMatches] = useState([]);
+
+    const teamName = "Delhi Capitals";
+
+
+    useEffect(
+      () => {
+        //asych/await systnx fits great with fetch() b/c it simplifies the work with promise
+        const fetchMatches = async () => {
+          const response = await fetch(`http://localhost:8080/team/{teamName}/matches?year=2019`);
+          const data = await response.json();
+          setMatches(data);
+        };
+
+        fetchMatches();
+
+      },[]
+    )
+
 
 
     return (
     <div className="MatchPage">
 
         <h1>Match Page</h1> 
+
+        {
+          matches.map(match => <MatchDetailCard teamName = {teamName} match = {match} />)
+        }
+
         
     
     </div>
   );
 }
+
 
 
